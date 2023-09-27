262a42bbbbe21231205a44a26dd0225dc108a25e
Muhammad Sinan <61152623+sinanLab@users.noreply.github.com>
2023-09-27 16:46:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from prob_calculator import Hat, experiment
 
-hat = Hat(blue=5, red=4, green=2)
-probability = experiment(hat=hat, expected_balls={"red": 1, "green": 2}, num_balls_drawn=4, num_experiments=10000)
-print(f"Probability: {probability}")
+hat = Hat(yellow=3, blue=2, green=6)
+balls_drawn = hat.draw(4)
+print(f"Balls drawn: {balls_drawn}")
