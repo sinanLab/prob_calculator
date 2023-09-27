a3fd39b78e6894f9158281b377bdb898f08131e7
Muhammad Sinan <61152623+sinanLab@users.noreply.github.com>
2023-09-27 16:46:46 +0800

    Update prob_calculator.py

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,5 +1,4 @@
 import random
-from collections import Counter
 
 class Hat:
     def __init__(self, **balls):
@@ -8,27 +7,25 @@ class Hat:
             self.contents.extend([color] * count)
 
     def draw(self, num_balls):
-        if num_balls >= len(self.contents):
-            return self.contents[:]
+        num_balls = min(num_balls, len(self.contents))
         balls_drawn = random.sample(self.contents, num_balls)
         for ball in balls_drawn:
             self.contents.remove(ball)
         return balls_drawn
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    successful_experiments = 0
-
+    match_count = 0
     for _ in range(num_experiments):
-        hat_copy = Hat(**hat.contents)
-        expected_balls_copy = dict(expected_balls)
+        hat_copy = Hat(**hat.__dict__)
         balls_drawn = hat_copy.draw(num_balls_drawn)
-
+        drawn_counts = {}
         for ball in balls_drawn:
-            if ball in expected_balls_copy:
-                expected_balls_copy[ball] -= 1
-
-        if all(value <= 0 for value in expected_balls_copy.values()):
-            successful_experiments += 1
-
-    probability = successful_experiments / num_experiments
-    return probability
+            drawn_counts[ball] = drawn_counts.get(ball, 0) + 1
+        match = True
+        for color, count in expected_balls.items():
+            if color not in drawn_counts or drawn_counts[color] < count:
+                match = False
+                break
+        if match:
+            match_count += 1
+    return match_count / num_experiments
